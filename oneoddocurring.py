4277931af1b75fdfc7541b8b8295e946227022d3
HD88-max <hardikdhallam@gmail.com>
2025-12-07 16:22:57 +0000

    Add files via upload

---
+++ oneoddocurring.py
@@ -0,0 +1,15 @@
+def Oddoccuring(arr):
+    res = 0
+    for element in arr:
+        res = res ^ element
+    return res
+
+
+arr = []
+n = int(input("Enter array size: "))
+while(n):
+    num = int(input("Enter number: "))
+    arr.append(num)
+    n = n -1
+
+print("Odd occurring number is", Oddoccuring((arr)))
